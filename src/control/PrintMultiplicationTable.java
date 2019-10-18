d3e50e316c13bdd9fd58c3889ab257488d324b74
yangli16 <yangli16@asiainfo.com>
2019-10-18 22:04:50 +0800

    提交

---
+++ src/control/PrintMultiplicationTable.java
@@ -0,0 +1,26 @@
+package control;
+/**
+ * @Author: yangli16
+ * @Description: 打印乘法表
+ * @Date: 22:03 2019/10/18
+ */
+public class PrintMultiplicationTable {
+    public static void main(String[] args) {
+        printTable();
+    }
+
+    /**
+     * @Author: yangli
+     * @Description: 打印9*9乘法表
+     * @Date: 22:03 2019/10/18
+     */
+    static void printTable(){
+        for (int i = 1; i <= 9 ; i++){
+            for (int j = 1; j <= i ; j++){
+                System.out.printf(j + "*" + i + "=" + (i * j));
+                System.out.printf(",");
+            }
+            System.out.println();
+        }
+    }
+}
